28627de1ac1af0b6383c5907af49c3c07a243170
Luis Balarezo <luis.balarezo@unmsm.edu.pe>
2023-07-06 07:32:35 -0500

    test

--- server.js
+++ server.js
@@ -6,6 +6,25 @@ const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+// Importamos el módulo 'express' para crear una aplicación de servidor.
+// Importamos el módulo 'next' para configurar y preparar la aplicación Next.js.
+// Importamos la función 'createProxyMiddleware' del módulo 'http-proxy-middleware' para crear un middleware de proxy.
+
+// Definimos una variable 'dev' que almacena si el entorno de ejecución es de desarrollo o producción.
+// Creamos una instancia de la aplicación Next.js pasando el valor de 'dev' al constructor.
+// Utilizamos la función 'getRequestHandler' para obtener el manejador de solicitudes de la aplicación.
+
+// Luego, llamamos al método 'prepare' de la instancia de la aplicación para prepararla y esperamos a que se complete.
+
+// A continuación, creamos una instancia de Express y la asignamos a la variable 'server'.
+
+// Si estamos en modo de desarrollo, configuramos un middleware de proxy para redirigir las solicitudes a la ruta "/api" al servidor en "http://localhost:8000".
+
+// Configuramos un manejador para todas las rutas que devuelve el manejador de solicitudes de la aplicación Next.js.
+
+// Finalmente, llamamos al método 'listen' en el servidor para iniciar el servidor en el puerto 3000 y mostramos un mensaje en la consola cuando el servidor esté listo.
+
+// Capturamos cualquier error que ocurra durante el proceso y lo mostramos en la consola.
 app
     .prepare()
     .then(() => {
@@ -20,6 +39,16 @@ app
             );
         }
 
+
+        // A continuación, creamos una instancia de Express y la asignamos a la variable 'server'.
+
+        // Si estamos en modo de desarrollo, configuramos un middleware de proxy para redirigir las solicitudes a la ruta "/api" al servidor en "http://localhost:8000".
+
+        // Configuramos un manejador para todas las rutas que devuelve el manejador de solicitudes de la aplicación Next.js.
+
+        // Finalmente, llamamos al método 'listen' en el servidor para iniciar el servidor en el puerto 3000 y mostramos un mensaje en la consola cuando el servidor esté listo.
+
+        // Capturamos cualquier error que ocurra durante el proceso y lo mostramos en la consola.
         server.all("*", (req, res) => {
             return handle(req, res);
         });
@@ -32,3 +61,18 @@ app
     .catch((err) => {
         console.log("Error", err);
     });
+
+
+// Definimos una variable 'dev' que almacena si el entorno de ejecución es de desarrollo o producción.
+// Creamos una instancia de la aplicación Next.js pasando el valor de 'dev' al constructor.
+// Utilizamos la función 'getRequestHandler' para obtener el manejador de solicitudes de la aplicación.
+
+// Luego, llamamos al método 'prepare' de la instancia de la aplicación para prepararla y esperamos a que se complete.
+
+// A continuación, creamos una instancia de Express y la asignamos a la variable 'server'.
+
+// Si estamos en modo de desarrollo, configuramos un middleware de proxy para redirigir las solicitudes a la ruta "/api" al servidor en "http://localhost:8000".
+
+// Configuramos un manejador para todas las rutas que devuelve el manejador de solicitudes de la aplicación Next.js.
+
+// Finalmente, llamamos al método 'listen' en el servidor para iniciar el servidor en el puerto 3000 y mostramos un mensaje en la consola cuando el servidor esté listo.
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,4 +1,13 @@
 // Definimos una función llamada currencyFormatter que toma un objeto de datos como argumento
+// Definiendo una función llamada currencyFormatter que acepta un objeto de datos como argumento y devuelve un valor formateado como una cadena de moneda.
+
+// En la función, multiplicamos el valor de 'amount' en el objeto de datos por 100 y luego lo dividimos por 100 para asegurarnos de que solo tenga dos decimales. Esto se hace para evitar problemas con los cálculos de redondeo en JavaScript.
+
+// Utilizamos el método toLocaleString para formatear el número según las convenciones de formato regional. Pasamos el código de idioma o región en el primer argumento y el código de moneda en el segundo argumento.
+
+// El resultado formateado se devuelve como una cadena de moneda.
+
+// Exportando la función currencyFormatter.
 export const currencyFormatter = (data) => {
     // Multiplicamos el monto por 100 y luego lo dividimos por 100 para asegurarnos de que solo tenga dos decimales
     // Esto se hace para evitar problemas con los cálculos de redondeo en JavaScript
@@ -8,6 +17,32 @@ export const currencyFormatter = (data) => {
         // El primer argumento es el código de idioma o la región que se utilizará para el formato
         style: "currency",
         // El segundo argumento es el código de moneda que se utilizará para el formato
+        // Este componente se utiliza para mostrar cada curso en la lista de cursos del usuario.
+
+        // Renderizando el contenido de cada curso mediante el uso de props.
+        // Muestra la imagen del curso utilizando el componente Avatar de Ant Design.
+        // Elige la imagen del curso de la ubicación proporcionada o muestra una imagen predeterminada si no hay imagen disponible.
+        // Muestra el nombre del curso como un enlace que redirige a la página de detalle del curso.
+        // Muestra el número de lecciones del curso y el nombre del instructor.
+
+        // Definiendo el componente UserIndex.
+
+        // Accediendo al estado global de la aplicación utilizando el hook useContext y desestructurando el estado "user" desde el objeto "state" del contexto.
+
+        // Utilizando el hook useState para crear una variable de estado llamada "courses" y "loading".
+        // "courses" se inicializa como un arreglo vacío para almacenar los cursos del usuario.
+        // "loading" se inicializa como falso para indicar que no se está cargando ningún dato en este momento.
+
+        // Utilizando el hook useEffect para realizar operaciones después de que el componente se haya montado.
+        // Define la función fetchCourses, que se utiliza para cargar los cursos del usuario.
+        // Llama a la función loadCourses para obtener los cursos del usuario.
+        // Llama a fetchCourses cuando el componente se monta para cargar los cursos del usuario.
         currency: data.currency,
     });
 };
+
+// Utilizamos el método toLocaleString para formatear el número según las convenciones de formato regional. Pasamos el código de idioma o región en el primer argumento y el código de moneda en el segundo argumento.
+
+// El resultado formateado se devuelve como una cadena de moneda.
+
+// Exportando la función currencyFormatter.
